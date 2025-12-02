d9cb044a4e824a71294f2f4048797416eba5b9b8
ezeiozzia91 <eze.iozzia@gmail.com>
2025-12-02 20:43:59 -0300

    "Installing icon dependency, creating footer and header component, updating principal routes"

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,5 +1,29 @@
+import { Link } from 'react-router-dom'
 import './Footer.css'
+import { FaArrowRightFromBracket, FaCircleUser, FaHouse } from 'react-icons/fa6'
 
+
+// Voy a tener que hacer el router dinámico basado en el tipo de usuario que haya logueado. 
 export const Footer = () => (
-  <footer>Footer</footer>
+  <footer className='footer'>
+    <nav>
+      <ul>
+        <li>
+          <Link to="/home-passenger" aria-label="Home">
+            <FaHouse className='footer-icon'/>
+          </Link>
+        </li>
+        <li>
+          <Link to="/profile-passenger" aria-label="Profile">
+            <FaCircleUser className='footer-icon'/>
+          </Link>
+        </li>
+        <li>
+          <Link to="/login" aria-label="Login">
+            <FaArrowRightFromBracket className='footer-icon'/>
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  </footer>
 )
--- src/routes/router/PrincipalRoutes.tsx
+++ src/routes/router/PrincipalRoutes.tsx
@@ -4,14 +4,18 @@ import { MainLayout } from '../../layouts/main/MainLayout'
 import { Home } from '../../pages/home/Home'
 import { Services } from '../../pages/services/Services'
 import { Contact } from '../../pages/contact/Contact'
+import { AuthLayout } from '../../layouts/auth/AuthLayout'
 
 export const PrincipalRoutes = () => {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path = "/auth" element = { <AuthLayout /> }>
+          {/* <Route path = "login" element = { <LoginPage /> }/> */}
+        </Route>
         <Route path = "/" element = { <MainLayout /> }>
-          <Route path = "home" element = { <Home /> }/>
-          <Route path = "services" element = { <Services /> }/>
+          <Route path = "home-passenger" element = { <Home /> }/>
+          <Route path = "profile-passenger" element = { <Services /> }/>
           <Route path = "contact" element = { <Contact /> }/>
         </Route>
       </Routes>
