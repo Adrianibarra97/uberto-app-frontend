ff0418b7546362d7c39e704e255a687034da25bd
ezeiozzia91 <eze.iozzia@gmail.com>
2025-12-06 16:42:41 -0300

    refactor title component

---
+++ src/components/title-component/TitleComponent.test.tsx
@@ -0,0 +1,5 @@
+describe('TitleComponent', () => {
+	it('App smoke TitleComponent', () => {
+		// render(<TitleComponent />)
+	})
+})
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,18 +1,19 @@
 import { DivisionComponent } from '../../components/division-component/DivisionComponent'
 import { FormPassenger } from '../../components/form-passenger/FormPassenger'
 import { GridComponent } from '../../components/grid/GridComponent'
+import { TitleComponent } from '../../components/title-component/TitleComponent'
 import './Home.css'
 
 export const Home = () => {
   return (
     <div className='main-layout'>
         <div className='home-container-items'>
-          <h1 className="title-component">Take a trip</h1>
+          <TitleComponent text='Take a Trip' />
           <FormPassenger />
         </div>
         <DivisionComponent />
         <div className='home-container-items'>
-          <h2 className='title-component'>Results</h2>
+          <TitleComponent text = 'Result' />
           <GridComponent />
         </div> 
     </div>
