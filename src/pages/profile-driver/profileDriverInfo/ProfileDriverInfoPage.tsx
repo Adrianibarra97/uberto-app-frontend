fbf5bb2dd0a822e1fc8258f4ea8fe0e6ca31973f
ezeiozzia91 <eze.iozzia@gmail.com>
2026-01-06 22:32:09 -0300

    profile driver page info

--- src/pages/profile-driver/profileDriverInfo/ProfileDriverInfoPage.tsx
+++ src/pages/profile-driver/profileDriverInfo/ProfileDriverInfoPage.tsx
@@ -1,3 +1,5 @@
+import { ButtonComponent } from "../../../components/button-component/ButtonComponent";
+import { DivisionComponent } from "../../../components/division-component/DivisionComponent";
 import "./ProfileDriverInfoPage.css";
 
 
@@ -6,7 +8,76 @@ export const ProfileDriverInfoPage = () => {
 
   return (
     <div className="profile-container-items">
-  
+      <form className="form-container-driver-profile">
+        <div className="form-item-driver-profile">
+          <label htmlFor="Name">Name</label>
+          <input 
+            type="text" 
+            id="Name" 
+            name="Name"
+            value="Ivan"
+            required
+          />
+        </div>
+        <div className="form-item-driver-profile" >
+          <label htmlFor="Surname">Surname</label>
+          <input 
+            type="text"
+            id="Surname" 
+            name="Surname"
+            value="Depineda"
+            required
+          />
+        </div>
+        <div className="form-item-driver-profile" >
+          <label htmlFor="Base Price">Base Price</label>
+          <input 
+            type="number"
+            id="Base Price" 
+            name="Base Price"
+            value="13000"
+            required
+          />
+        </div>
+      </form>
+
+      <DivisionComponent />
+      <h4 className="title-driver-profile">Premium Driver</h4>
+      <form className="form-container-driver-profile">
+        <div className="form-item-driver-profile">
+          <label htmlFor="Name">Name</label>
+          <input 
+            type="text" 
+            id="Name" 
+            name="Name"
+            value="Ivan"
+            required
+          />
+        </div>
+        <div className="form-item-driver-profile" >
+          <label htmlFor="Surname">Surname</label>
+          <input 
+            type="text"
+            id="Surname" 
+            name="Surname"
+            value="Depineda"
+            required
+          />
+        </div>
+        <div className="form-item-driver-profile" >
+          <label htmlFor="Base Price">Base Price</label>
+          <input 
+            type="number"
+            id="Base Price" 
+            name="Base Price"
+            value="13000"
+            required
+          />
+        </div>
+      </form>
+      <div className="button-container-driver-profile">
+        <button className="button-component-driver-profile">Save Changes</button>
+      </div>
     </div>
       
   );
