d011fd9d7cf123f90b071db4701c10f14f1f4fda
ezeiozzia91 <eze.iozzia@gmail.com>
2026-01-08 20:49:52 -0300

    services structure

---
+++ src/services/auth-service/AuthServiceInter.ts
@@ -0,0 +1,28 @@
+import { AuthCredentialsLoginDTO } from '../../domain/User'
+import { DRIVER_TYPE, PASSENGER_TYPE, USER_ID_TOKEN } from '../config'
+
+
+export abstract class AuthServiceInter {
+
+	public userType: string = ''
+	
+	login(authCredentialsLoginDTO: AuthCredentialsLoginDTO): void {
+		console.log(authCredentialsLoginDTO)
+	}
+
+	logout(): void {
+		localStorage.clear()
+	}
+
+	isAuthorized(): boolean {
+		return localStorage.getItem(USER_ID_TOKEN) !== null
+	}
+
+	isDriver(): boolean {
+		return this.userType === DRIVER_TYPE
+	}
+
+	isPassenger(): boolean {
+		return this.userType === PASSENGER_TYPE
+	}
+}
---
+++ src/services/passenger-service/PassengerServiceManager.ts
@@ -0,0 +1,19 @@
+import { PassengerService } from "./PassengerService"
+import type { PassengerServiceInter } from "./PassengerServiceInter"
+import { PassengerServiceStub } from "./PassengerServiceStub"
+
+
+class PassengerServiceManager {
+
+	private static instance: PassengerServiceInter
+	private static useStub: boolean = true
+
+	public static getIntance(): PassengerServiceInter {
+		if(!PassengerServiceManager.instance) {
+			PassengerServiceManager.instance = this.useStub ? new PassengerServiceStub() : new PassengerService()
+		}
+		return PassengerServiceManager.instance
+	}
+}
+
+export default PassengerServiceManager
