00b7f70fc4399bf8a5963b8f14b306e2c6a0bb95
ezeiozzia91 <eze.iozzia@gmail.com>
2026-01-13 21:19:43 -0300

    Trip domain

---
+++ src/domain/Trip.ts
@@ -0,0 +1,36 @@
+export type TripJSON = {
+    id: number,
+    origin: string,
+    destination: string
+    date: string,
+    amountOfPassengers: number
+}
+
+export class Trip {
+    constructor (
+        public id: number = -1,
+        public origin: string = '',
+        public destination: string = '',
+        public date: string = '',
+        public amountOfPassengers: number = 0
+    ) {}
+
+    static fromJSON(tripJSON: TripJSON): Trip {
+        return new Trip (
+            tripJSON.id, tripJSON.origin, tripJSON.destination,tripJSON.date,tripJSON.amountOfPassengers
+        )
+    }
+
+    static toJSON(): TripJSON {
+        return {
+            id: this.id,
+            origin: this.origin,
+            destination: this.destination,
+            date: this.date,
+            amountOfPassengers: this.amountOfPassengers
+
+        }
+    }
+
+
+}
--- src/services/trip-service/TripServiceInter.ts
+++ src/services/trip-service/TripServiceInter.ts
@@ -1,3 +1,14 @@
+import type { Trip } from "../../domain/Trip";
+
 export abstract class TripServiceInter {
 
+    getAll(): Promise<Trip[]>
+
+    getOneById(id: number): Promise<Trip>
+
+    create(passenger: Trip): Promise<void>
+
+    update(passenger: Trip): Promise<void>
+
+    delete(id: number): Promise<void>
 }
